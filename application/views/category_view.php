7d42e310f52885edf75ed1642aff7b0b1ac89dbc
manukam <maduranga.manuka3@gmail.com>
2018-11-02 01:26:00 +0530

    category view ui

--- application/views/category_view.php
+++ application/views/category_view.php
@@ -9,21 +9,21 @@
   <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
   <!-- <script src="main.js"></script> -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="<?php echo asset_url(). 'css/materialize.css'?>" type="text/css" rel="stylesheet" media="screen,projection" />
-  <link href="<?php echo asset_url(). 'css/style.css'?>" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="<?php echo asset_url() . 'css/materialize.css' ?>" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="<?php echo asset_url() . 'css/style.css' ?>" type="text/css" rel="stylesheet" media="screen,projection" />
 </head>
 
 <body>
-  <?php include('navbar.php');
-    ?>
-      <div class ="container">
+  <?php include 'navbar.php';
+?>
+  <div class="container">
     <div class="section">
-  <p class="category-heading "> Biographies & Memoirs </p>
-  <p class="category-description">Find your new favorite book in Biographies or
-    Memoirs. Plus, see our picks for the best Biography and Memoir of the month. </p>
-</div>
-</div>  
-  
+      <p class="category-heading "> Biographies & Memoirs </p>
+      <p class="category-description">Find your new favorite book in Biographies or
+        Memoirs. Plus, see our picks for the best Biography and Memoir of the month. </p>
+    </div>
+  </div>
+
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container">
@@ -38,47 +38,114 @@
 
       </div>
     </div>
-    <div class="parallax"><img src=<?php echo asset_url(). 'images/biography_cat.jpg' ?> alt="Unsplashed background img 1"></div>
+    <div class="parallax"><img src=<?php echo asset_url() . 'images/biography_cat.jpg' ?> alt="Unsplashed background img
+      1"></div>
   </div>
 
+  <div class="container">
+    <div class="section">
+      <h5> Shop By Category </h5>
 
-  <h5> Most viewed Biographies and Memoirs </h5>
-  <div class="horizontal-scroll">
-  <?php foreach($category_books as $m_viewed_book){
+      <div class="row">
+        <div class="col s12 m3">
+          <div class="card small category-card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="<?php echo asset_url() . 'images/books/mamba.jpg' ?>">
+            </div>
+            <div class="card-content">
+              <span class="card-title category-name activator grey-text text-darken-4">Memoirs</span>
+              <!-- <p><a href="#">Browse</a></p> -->
+            </div>
+          </div>
+        </div>
+
+          <div class="col s12 m3">
+            <div class="card small category-card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo asset_url() . 'images/books/fear.jpg' ?>">
+              </div>
+              <div class="card-content">
+                <span class="card-title category-name activator grey-text text-darken-4">Historical</span>
+                <!-- <p><a href="#">Browse</a></p> -->
+              </div>
+            </div>
+        </div>
+
+        <div class="col s12 m3">
+            <div class="card small category-card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo asset_url() . 'images/books/coldblood.jpg' ?>">
+              </div>
+              <div class="card-content">
+                <p class="card-title category-name activator grey-text text-darken-4">True Crime</p>
+                <!-- <p><a href="#">Browse</a></p> -->
+              </div>
+            </div>
+        </div>
+
+        <div class="col s12 m3">
+            <div class="card small category-card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo asset_url() . 'images/books/moneyball.jpg' ?>">
+              </div>
+              <div class="card-content">
+                <p class="card-title category-name activator grey-text text-darken-4">Sports & Outdoors</p>
+                <!-- <p><a href="#">Browse</a></p> -->
+              </div>
+            </div>
+        </div>
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <!-- <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5> -->
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src=<?php echo asset_url() . 'images/image7.jpg' ?> alt="Unsplashed background img 2"></div>
+  </div>
+
+      <h5> Most viewed Biographies and Memoirs </h5>
+      <div class="horizontal-scroll">
+        <?php foreach ($category_books as $m_viewed_book) {
 
     ?>
-  <div class="card medium custom-height hover-reveal">
-    <div class="card-image">
-      <img class="activator resize" src="<?php echo asset_url(). 'images/books/history.jpg'?>">
-    </div>
-    <div class="card-content">
-      <span class="card-title bname">
-        <?php echo $m_viewed_book->book_name; ?></span>
-      <span class="card-content ">
-        <?php echo $m_viewed_book->author_name; ?></span>
+        <div class="card medium custom-height hover-reveal">
+          <div class="card-image">
+            <img class="activator resize" src="<?php echo asset_url() . 'images/books/history.jpg' ?>">
+          </div>
+          <div class="card-content">
+            <span class="card-title bname">
+              <?php echo $m_viewed_book->book_name; ?></span>
+            <span class="card-content ">
+              <?php echo $m_viewed_book->author_name; ?></span>
 
-      <hr>
-      <i class="material-icons">attach_money</i>35.00
-      <p><a href="#">Add to Cart</a></p>
+            <hr>
+            <i class="material-icons">attach_money</i>35.00
+            <p><a href="#">Add to Cart</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">
+              <?php echo $m_viewed_book->book_name; ?><i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once hover it.</p>
+            <p>Lorem ipsum dolor sit amet, et ius nostro pertinacia, nihil congue temporibus vel cu. Senserit
+              petentium cum te. Ullum inermis quaerendum ex ius, sed nisl insolens te. Eu dicit laoreet sea.
+              summo epicuri dissentiunt ad vel. At eam malorum percipitur, ei debet aperiri indoctum cum, tantas
+              omittam ad nam. Eu per erant putent, eu has suas sale.</p>
+          </div>
+        </div>
+        <!-- </div> -->
+        <?php }?>
+      </div>
+
+     </div>
     </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">
-        <?php echo $m_viewed_book->book_name; ?><i class="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once hover it.</p>
-      <p>Lorem ipsum dolor sit amet, et ius nostro pertinacia, nihil congue temporibus vel cu. Senserit
-        petentium cum te. Ullum inermis quaerendum ex ius, sed nisl insolens te. Eu dicit laoreet sea.
-        summo epicuri dissentiunt ad vel. At eam malorum percipitur, ei debet aperiri indoctum cum, tantas
-        omittam ad nam. Eu per erant putent, eu has suas sale.</p>
-    </div>
-  </div>
-  <!-- </div> -->
-  <?php } ?>
-  </div>
 </body>
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/velocity-animate@1.5/velocity.min.js"></script>
-<script src="<?php echo asset_url(). 'js/materialize.js'?>"></script>
-<script src="<?php echo asset_url(). 'js/init.js'?>"></script>
-<script src="<?php echo asset_url(). 'js/custom.js'?>"></script>
+<script src="<?php echo asset_url() . 'js/materialize.js' ?>"></script>
+<script src="<?php echo asset_url() . 'js/init.js' ?>"></script>
+<script src="<?php echo asset_url() . 'js/custom.js' ?>"></script>
 
 </html>
