59f4f77f931bdc86832d43ac50685743cc0e0327
manukam <maduranga.manuka3@gmail.com>
2018-11-13 00:30:14 +0530

    renaming

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,127 @@
+<?php
+
+class AdminController extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model("BookModel");
+        $this->load->model("AdminModel");
+    }
+
+    public function loginView(){
+        $this->load->view("admin_login");
+    }
+
+    public function login(){
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        if($username === 'admin' && $password === '1234'){
+            redirect('index.php/Admin_controller/show_dashboard/');
+        }else{
+            // alert("wrong info");
+        }
+    }
+
+    public function showDashboard(){
+        
+        $data['mostViewed'] = $this->BookModel->getMostViewedBookList();
+        $data['allBooks'] = $this->BookModel->getAllBooks();
+
+        $categoryViews = $this->AdminModel->getViewedCategories();
+        $mostViewedCategories = array();
+        foreach($categoryViews as $index=>$views){
+            $mostViewedCategories[$index] = $views->NUM;
+        }
+        $data['mostViewedCategories'] = $mostViewedCategories;
+        $data['bookCategories'] = $this->BookModel->getBookCategories();
+        $data['totalPageViews'] = $this->AdminModel->getTotalPageViews();
+        $data['totalVisitors'] = $this->AdminModel->getVisitors();
+        $data['visitorCountPerDay'] = $this->AdminModel->getVisitorCountPerDay();
+
+
+        $data['authors'] = $this->Admin_model->getAuthors();
+        $this->load->view('AdminPage',$data);
+    }
+
+    public function getSubCategories($mainCategoryId){
+        $subCategories = $this->AdminModel->getSubCategories($mainCategoryId);
+        $result= array();
+        foreach($subCategories as $index=>$sub){
+            $result[$sub->id] = $sub->subCatName;     
+         }
+         echo json_encode($result);
+    }
+
+    public function addBook(){
+        $bookName = $this->input->post('bookName');
+        $author = $this->input->post('author');
+        $mainCat = $this->input->post('mainCat');
+        $subCat = $this->input->post('subCat');
+        $description = $this->input->post('description');
+        $price = $this->input->post('bookPrice');
+        // var_dump($book_name);
+
+        $data = array('book_name'=> $bookName,
+                        "book_author" => $author,
+                    'book_cat'=>$mainCat,
+                'sub_cat'=>$subCat,
+                'description'=>$description,
+                'price'=>$price
+            );
+        $insertId = $this->BookModel->insertBook($data);
+
+        echo $insertId;
+    }
+
+    public function addBookImage($fileName){
+        $config['upload_path']= "./assets/images/books/";
+        $config['allowed_types']='jpg|png';
+        $config['encrypt_name'] = FALSE;
+        $config['file_name'] = $fileName;
+        $config['max_size']  = 1000;
+         
+        $this->upload->initialize($config);
+        if($this->upload->do_upload("book_image")){
+            $data = array('upload_data' => $this->upload->data());
+            print_r('uploaded');
+            redirect('AdminController/showDashboard#t2');
+        }else{
+            $response = $this->upload->display_errors();
+        }
+    }
+
+    public function addCategoryImage($fileName){
+        $config['upload_path']= "./assets/images/Category/";
+        $config['allowed_types']='jpg|png';
+        $config['encrypt_name'] = FALSE;
+        $config['file_name'] = $fileName;
+        $config['max_size']  = 1000;
+         
+        $this->upload->initialize($config);
+        if($this->upload->do_upload("category_image")){
+            $data = array('upload_data' => $this->upload->data());
+            redirect('AdminController/showDashboard#t2');
+        }else{
+            $response = $this->upload->display_errors();
+            print_r ($response);
+        }
+    }
+
+    public function addCategory(){
+        $mainCat = $this->input->post("mainCat");
+        $subCats = $this->input->post('subCats');
+
+        // var_dump($sub_cats);die;
+        $insertCatId = $this->Admin_model->add_category($main_cat,$sub_cats);
+        echo $insertCatId;
+    }
+
+    public function bookValidate(){
+        $bookName = $this->input->post("book_name");
+        $result = $this->bookModel->checkBook($bookName);
+        // print_r($result);
+        echo $result;
+    }
+}
+
+?>
---
+++ application/controllers/BookController.php
@@ -0,0 +1,44 @@
+<?php
+
+class BookController extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model("BookModel");
+    }
+
+
+    public function viewBook($book_id){
+        $userId = $this->session->userdata('user_id'); 
+        $this->ViewTrackingModel->trackViews($bookId,$userId); 
+
+        $relatedBooksAll = $this->BookModel->getRelatedBooks($bookId);
+        $relatedBooksFiltered = array();
+
+        foreach($relatedBooksAll as  $book){
+            if($book->id !== $bookId){
+                array_push($relatedBooksFiltered, $book); 
+            }
+        }
+
+        $data['relatedBooks'] = $relatedBooksFiltered;
+
+
+        $data['recentBooks'] = $this->getRecent();
+
+        $data['bookDetails'] = $this->BookModel->getBookDetails($bookId);
+        
+
+
+        $this->load->view("BookView",$data);
+    }
+
+    public function getRecent(){
+        $userId = $this->session->userdata('userId');
+        return $this->BookModel->getRecentBooks($userId);
+    }
+
+    
+
+}
+?>
